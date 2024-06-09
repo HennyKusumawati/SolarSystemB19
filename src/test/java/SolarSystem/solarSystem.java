52ac0dc38da7c5220d4df88e003e46460531203b
HennyKusumawati <anayank@gmail.com>
2024-06-08 22:01:51 -0400

    planet2 is done

--- src/test/java/SolarSystem/solarSystem.java
+++ src/test/java/SolarSystem/solarSystem.java
@@ -7,10 +7,16 @@ public class solarSystem {
         solar.planet="2";
         solar.star="2";
         System.out.println("the solar system is ready");
-       //feature Sun by Fabricio
+       //featureSun by Fabricio
         featureSun xros=new featureSun();
         xros.color="pink";
         xros.temp="24324kv";
         xros.size="0km";
+
+        //planet2 by Alex
+        planet2 perox=new planet2();
+        perox.color="green";
+        perox.size="1m";
+        perox.radius="3m";
     }
 }
